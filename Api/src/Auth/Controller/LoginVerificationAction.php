9d1c8a77eb3a90c6f0cbe1262129ff9cdc639cd7
Jarosław Murawski <verymysteriusmen@gmail.com>
2022-01-05 13:37:01 +0100

    endpoint na login

---
+++ Api/src/Auth/Controller/LoginVerificationAction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Auth\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Security;
+
+class LoginVerificationAction extends AbstractController
+{
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function __invoke(): JsonResponse
+    {
+        $user = $this->security->getUser();
+        return new JsonResponse(["username" => $user->getUserIdentifier()], Response::HTTP_OK);
+    }
+}
